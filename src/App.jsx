20ec651b6f93a8da136ddaefadd5ef7be779fb96
chathudilzo <chathuradilshanudawaththa@gmail.com>
2025-07-02 21:16:48 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="flex-center h-[100vh]">Hello GSAP</h1>
+    </div>
+  );
+};
+
+export default App;
